687fb76d7f0eff282b32b6383cb85d67e1565497
Maged <maged.habb87@gmail.com>
2017-10-17 16:36:57 -0700

    I added ex3

---
+++ ex3/ex3.py
@@ -0,0 +1,13 @@
+my_name = "Maged"
+my_age = 30
+my_height = 74
+my_weight = 180
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print("let's talk about %s" %my_name)
+print("He's %d inches tall" %my_height)
+print("He's %d pounds heavy" %my_weight)
+print("he's got %s eyes and %s hair" %(my_eyes, my_hair))
+print("If I add %d, %d, and %d I get %d" %(my_age, my_height, my_weight, my_age + my_height+ my_weight))
